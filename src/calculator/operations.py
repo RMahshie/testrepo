b9c85119e1e53e3407ed4473a9c74466ed260789
Ryan <ryan@mahshie.net>
2025-12-18 14:49:07 -0500

    stuff

--- src/calculator/operations.py
+++ src/calculator/operations.py
@@ -6,6 +6,7 @@
 #hello again again again
 #hello again again again again
 #hello again again again again again
+#hello again again again again again again
 def add(a, b):
     """
     Add two numbers.
